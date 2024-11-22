d5d6affb664af79176ca20b16e801958120c2288
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2024-11-22 11:54:06 +0300

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -12,3 +12,17 @@ def test_gendiff_json():
 def test_gendiff_yaml():
     got = generate_diff('./tests/fixtures/file1.yml', './tests/fixtures/file2.yaml')
     assert got == expected
+
+
+with open('./tests/fixtures/expected_tree_output.txt', 'r', encoding="utf-8") as file:
+    expected = file.read()
+
+
+def test_gendiff_tree_json():
+    got = generate_diff('./tests/fixtures/file1_tree.json', './tests/fixtures/file2_tree.json')
+    assert got == expected
+
+
+def test_gendiff_tree_yaml():
+    got = generate_diff('./tests/fixtures/file1_tree.yaml', './tests/fixtures/file2_tree.yaml')
+    assert got == expected
