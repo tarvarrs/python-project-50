9352e15ebf08f73ea6f3b52a8300199493d73eb0
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2025-01-12 18:03:37 +0300

    correct linting

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -28,7 +28,7 @@ def main():
         usage='%(prog)s [-h] [-f FORMAT] first_file second_file',
         description='Compares two configuration files and shows a difference.',
         formatter_class=argparse.RawDescriptionHelpFormatter
-        )
+    )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
